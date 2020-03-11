7d56fc8bcf27fd3f92b487fa240962ef394ce4cd
Ilia Mazlov <vfpkjd@gmail.com>
2020-03-11 23:07:33 +0100

    init commit: register hotkey; custom button; min/max window

---
+++ Tags/MainWindow.xaml.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Interop;
+using System.Runtime.InteropServices;
+
+namespace Tags
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+
+    public partial class MainWindow : Window
+    {
+        [DllImport("user32.dll")]
+        private static extern bool RegisterHotKey(IntPtr hWnd, int id, uint fsModifiers, uint vk);
+        [DllImport("user32.dll")]
+        private static extern bool UnregisterHotKey(IntPtr hWnd, int id);
+
+        private HwndSource _source;
+        private const int HOTKEY_ID = 9000;
+
+        protected override void OnSourceInitialized(EventArgs e)
+        {
+            base.OnSourceInitialized(e);
+            var helper = new WindowInteropHelper(this);
+            _source = HwndSource.FromHwnd(helper.Handle);
+            _source.AddHook(HwndHook);
+            RegisterHotKey();
+        }
+
+        protected override void OnClosed(EventArgs e)
+        {
+            _source.RemoveHook(HwndHook);
+            _source = null;
+            UnregisterHotKey();
+            base.OnClosed(e);
+        }
+
+        private void RegisterHotKey()
+        {
+            var helper = new WindowInteropHelper(this);
+            const uint VK_F10 = 0x79;
+            const uint MOD_CTRL = 0x0002;
+            if (!RegisterHotKey(helper.Handle, HOTKEY_ID, MOD_CTRL, VK_F10))
+            {
+                // handle error
+            }
+        }
+        
+private void UnregisterHotKey()
+{
+    var helper = new WindowInteropHelper(this);
+    UnregisterHotKey(helper.Handle, HOTKEY_ID);
+}
+
+        enum KeyModifier
+        {
+            None = 0,
+            Alt = 1,
+            Control = 2,
+            Shift = 4,
+            WinKey = 8
+        }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            btnTower.Click += BtnTower_Click;
+        }
+
+        private void BtnTower_Click(object sender, RoutedEventArgs e)
+        {
+            btnFrisbee.Content = "Heyhey";
+            WindowState = WindowState.Maximized;
+        }
+
+        private IntPtr HwndHook(IntPtr hwnd, int msg, IntPtr wParam, IntPtr lParam, ref bool handled)
+        {
+            const int WM_HOTKEY = 0x0312;
+            switch (msg)
+            {
+                case WM_HOTKEY:
+                    switch (wParam.ToInt32())
+                    {
+                        case HOTKEY_ID:
+                            OnHotKeyPressed();
+                            handled = true;
+                            break;
+                    }
+                    break;
+            }
+            return IntPtr.Zero;
+        }
+
+        private void OnHotKeyPressed()
+        {
+            btnFrisbee.Content = "Heyhey2";
+            WindowState = WindowState.Normal;
+            Activate();
+        }
+    }
+}
---
+++ WindowsExplorerSelectedFiles/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Shell32;
+using System.IO;
+using SHDocVw;
+
+namespace WindowsExplorerSelectedFiles
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string filename;
+            ArrayList selected = new ArrayList();
+            ShellWindows winClass = new ShellWindows();
+            int cnt = 1;
+            foreach (SHDocVw.InternetExplorer window in winClass)
+            {
+                filename = Path.GetFileNameWithoutExtension(window.FullName).ToLower();
+                if (filename.ToLowerInvariant() == "explorer")
+                {
+                    Console.WriteLine("Windows Explorer Instance #{0}:", cnt++);
+                    Shell32.FolderItems items = ((Shell32.IShellFolderViewDual2)window.Document).SelectedItems();
+                    foreach (Shell32.FolderItem item in items)
+                        selected.Add(item.Path);
+                    if (selected.Count > 0)
+                        foreach (var path in selected)
+                            Console.WriteLine(path);
+                    else
+                        Console.WriteLine("No files selected");
+
+                    Console.WriteLine();
+                }
+                selected.Clear();
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
